846eb1c9e8f449ed49e2a4248410f5259cf917e6
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-11 20:36:27 -0700

    displayed posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import faker from "faker";
 
 export class PostList extends Component {
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <img
+            src={faker.image.avatar()}
+            alt="user avatar"
+            className="ui image"
+          />
+          <div className="content">
+            <div className="header">{post.title}</div>
+            <div className="description">{post.body}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   componentDidMount() {
     this.props.fetchPosts();
   }
+
   render() {
-    return <div>PostsList</div>;
+    return (
+      <div className="ui very relaxed divided list">{this.renderList()}</div>
+    );
   }
 }
 
-const mapStateToProps = state => {};
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
 
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
